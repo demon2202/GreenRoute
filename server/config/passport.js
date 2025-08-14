582523cec8eb125f1ea5a944cbc710e0a58ed991
Harshit Singh Negi <142771674+demon2202@users.noreply.github.com>
2025-08-14 14:52:10 +0530

    Update passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,17 +1,51 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/User');
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => {
-    User.findById(id).then(user => {
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
         done(null, user);
-    });
+    } catch (err) {
+        done(err, null);
+    }
 });
 
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+}, async (email, password, done) => {
+    try {
+        const user = await User.findOne({ 
+            email: email.toLowerCase() 
+        });
+
+        if (!user) {
+            return done(null, false, { message: 'No user found with that email address' });
+        }
+
+        if (!user.password) {
+            return done(null, false, { message: 'Please sign in with Google or reset your password' });
+        }
+
+        // Verify password
+        const isMatch = await user.matchPassword(password);
+        if (!isMatch) {
+            return done(null, false, { message: 'Incorrect password' });
+        }
+
+        return done(null, user);
+    } catch (err) {
+        return done(err);
+    }
+}));
+
+// Google OAuth Strategy
 passport.use(
     new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
@@ -20,16 +54,32 @@ passport.use(
         proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
-        const existingUser = await User.findOne({ googleId: profile.id });
-        if (existingUser) {
-            return done(null, existingUser);
+        try {
+            const existingUser = await User.findOne({ 
+                $or: [
+                    { googleId: profile.id },
+                    { email: profile.emails[0].value.toLowerCase() }
+                ]
+            });
+
+            if (existingUser) {
+                if (!existingUser.googleId) {
+                    existingUser.googleId = profile.id;
+                    await existingUser.save();
+                }
+                return done(null, existingUser);
+            }
+
+            const user = await new User({
+                googleId: profile.id,
+                displayName: profile.displayName,
+                email: profile.emails[0].value.toLowerCase(),
+                image: profile.photos[0].value
+            }).save();
+            
+            done(null, user);
+        } catch (err) {
+            done(err, null);
         }
-        const user = await new User({
-            googleId: profile.id,
-            displayName: profile.displayName,
-            email: profile.emails[0].value,
-            image: profile.photos[0].value
-        }).save();
-        done(null, user);
     })
 );
